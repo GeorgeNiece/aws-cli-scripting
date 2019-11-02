715708e9c98c511d9724f028b4aea0d4ad8530fa
George Niece <George.Niece@digitaltransformationstrategies.net>
2019-11-02 21:52:30 +0000

    update deletion of users with various elements

--- deleteUsers.sh
+++ deleteUsers.sh
@@ -1,4 +1,25 @@
 for users in `aws iam list-users  --output text | cut -f7`
 do 
+   echo "Removing $users from groups"
+   for groups in `aws iam list-groups-for-user --user-name $users --output text | cut -f5`
+      do
+        echo "Removing $users from group $groups"
+	aws iam remove-user-from-group --group-name $groups --user-name $users
+      done
+   echo "Detaching all policies from user $users"
+   for policies in `aws iam list-attached-user-policies --user-name $users --output text | cut -f2`
+      do
+        echo "Detaching policy $policies from  $users "
+        aws iam detach-user-policy --user-name $users --policy-arn $policies
+      done
+   echo "Deleting all access keys from user $users "
+   for access_keys in `aws iam list-access-keys --user-name $users --output text | cut -f2`
+      do
+        echo "Deleting access key $access_keys for  $users "
+        aws iam delete-access-key --user-name $users --access-key-id $access_keys
+      done
+   echo "Removing login profile for $users"
+   aws iam list-groups-for-user --user-name $users --output text
    echo "Removing user $users"
+   aws iam delete-user --user-name $users
 done
