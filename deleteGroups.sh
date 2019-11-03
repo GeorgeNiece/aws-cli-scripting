35c2891bc84f45720668255c23efb06eada2c160
George Niece <george.niece@gmail.com>
2019-11-03 06:52:09 +0000

    group cleanup for inline policy attachment

--- deleteGroups.sh
+++ deleteGroups.sh
@@ -1,11 +1,17 @@
 for groups in `aws iam list-groups --output text | cut -f5`
 do 
-    echo "Detaching all policies from user $groups"
+    echo "Detaching managed policies from group $groups"
    for policies in `aws iam list-attached-group-policies --group-name $groups --output text | cut -f2`
       do
         echo "Detaching policy $policies from  $groups"
         aws iam detach-group-policy --group-name $groups --policy-arn $policies
       done
-   echo "Removing $users from groups"
+    echo "Detaching inline policies from group $groups"
+   for policies in `aws iam list-group-policies --group-name $groups --output text | cut -f2`
+      do
+        echo "Detaching policy $policies from  $groups"
+        aws iam delete-group-policy --group-name $groups --policy-name $policies
+      done
+   echo "Removing $groups from groups"
    aws iam delete-group --group-name $groups 
 done
