72adb8c0c754d33147402f72fd00db5c18bb779f
George Niece <george.niece@gmail.com>
2019-11-03 04:30:19 +0000

    update cloud9 cleanup

--- deleteInstances.sh
+++ deleteInstances.sh
@@ -13,3 +13,9 @@ do
         --region $region \
         --instance-id {}
 done
+## don't know how to run this alongside the instance deletion" 
+for cloud9envs in `aws cloud9 list-environments --output text | cut -f2`
+do
+    echo "Delete cloud9 environment $cloud9envs"
+    aws cloud9 delete-environment --environment-id $cloud9_env
+done
