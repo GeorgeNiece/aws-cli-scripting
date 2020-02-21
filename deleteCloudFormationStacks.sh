fc585b4f715160594075f78dfb52a56ac4c806ce
George Niece <george.niece@gmail.com>
2020-02-21 18:53:12 +0000

    updates for multi-region CloudFormation removal

--- deleteCloudFormationStacks.sh
+++ deleteCloudFormationStacks.sh
@@ -1,4 +1,6 @@
-for stack in `aws cloudformation list-stacks --output text | cut -f5`
+for region in us-east-1 us-east-2 us-west-1 us-west-2
+do
+for stack in `aws --region $region cloudformation list-stacks --output text | cut -f4`
 do 
    echo "Processing CloudFormation stack $stack"
   if [[ "$stack" != *"cloud9"* ]];
@@ -8,4 +10,5 @@ do
    else
 	echo "Not removing cloud9 $stack"
   fi
+done
 done
