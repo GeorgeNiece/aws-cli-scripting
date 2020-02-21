63a84d540f2f53cba73845ea6844d5b0b0201d02
George Niece <george.niece@gmail.com>
2020-02-21 18:29:59 +0000

    updated CloudFormation not to remove Cloud9

--- deleteCloudFormationStacks.sh
+++ deleteCloudFormationStacks.sh
@@ -1,5 +1,11 @@
 for stack in `aws cloudformation list-stacks --output text | cut -f5`
 do 
-   echo "Removing CloudFormation stack $stack"
-   aws cloudformation delete-stack --stack-name "${stack}"
+   echo "Processing CloudFormation stack $stack"
+  if [[ "$stack" != *"cloud9"* ]];
+	then
+		echo " Removing $stack"
+		aws cloudformation delete-stack --stack-name "${stack}"
+   else
+	echo "Not removing cloud9 $stack"
+  fi
 done
