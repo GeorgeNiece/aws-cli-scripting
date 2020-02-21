0c9a73601607304984a5ad96986e0462e45967fe
George Niece <george.niece@gmail.com>
2020-02-21 21:11:15 +0000

    update for variable cloudformation naming

--- deleteCloudFormationStacks.sh
+++ deleteCloudFormationStacks.sh
@@ -12,3 +12,17 @@ do
   fi
 done
 done
+for region in us-east-1 us-east-2 us-west-1 us-west-2
+do
+for stack in `aws --region $region cloudformation list-stacks --output text | cut -f6`
+do 
+   echo "Processing CloudFormation stack $stack"
+  if [[ "$stack" != *"cloud9"* ]];
+	then
+		echo " Removing: aws cloudformation delete-stack --stack-name $stack"
+		aws cloudformation delete-stack --region $region --stack-name $stack
+   else
+	echo "Not removing cloud9 $stack"
+  fi
+done
+done
