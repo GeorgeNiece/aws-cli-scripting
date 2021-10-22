378fd5faa4da557b94751502839842eba1fbc282
George Niece <george.niece@gmail.com>
2021-10-22 17:03:40 +0000

    Updates for iot

--- setupClean.sh
+++ setupClean.sh
@@ -6,8 +6,8 @@ chmod 755 jq
 cd ~/environment/aws-cli-scripting 
 chmod 755 *.sh
 echo "add the following lines to the .bashrc with the access key generated for the root account for the specific account"
-echo "export AWS_ACCESS_KEY_ID=" >> ~/.bashrc
-echo "export AWS_SECRET_ACCESS_KEY=" >> ~/.bashrc
+echo "#export AWS_ACCESS_KEY_ID=" >> ~/.bashrc
+echo "#export AWS_SECRET_ACCESS_KEY=" >> ~/.bashrc
 echo "git config --global user.name 'George Niece' " >>  ~/.bashrc
 echo "git config --global user.email george.niece@gmail.com" >>  ~/.bashrc
 echo "set -o vi" >>  ~/.bashrc
