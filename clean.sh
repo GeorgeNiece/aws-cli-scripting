e714fa75f89b9a4f553b9b6716d204eb7173d9e1
George Niece <george.niece@gmail.com>
2021-10-27 02:41:26 +0000

    Update DynamoDB handling

--- clean.sh
+++ clean.sh
@@ -1,7 +1,9 @@
 ./disableCloudFront.sh
+sleep 120 
 ./deleteBuckets.sh
 ./deleteCloudFormationStacks.sh
 ./deleteUsers.sh
 ./deleteGroups.sh
 ./deleteCloudFront.sh
+./deleteDynamodbTables.sh
 ./deleteInstances.sh
