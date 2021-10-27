ac42d19fff7ee8915390397af6e9e0a616b22b8b
George Niece <george.niece@gmail.com>
2021-10-27 22:40:27 +0000

    CloudFormation update

--- deleteCloudFormationStacks.sh
+++ deleteCloudFormationStacks.sh
@@ -1,6 +1,6 @@
 for region in us-east-1 us-east-2 us-west-1 us-west-2
 do
-for stack in `aws --region $region cloudformation list-stacks --output text | cut -f4`
+for stack in `aws --region $region cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --output text | grep STACKSUMMARIES | cut -f4`
 do 
    echo "Processing CloudFormation stack $stack"
   if [[ "$stack" != *"cloud9"* ]];
@@ -14,7 +14,7 @@ done
 done
 for region in us-east-1 us-east-2 us-west-1 us-west-2
 do
-for stack in `aws --region $region cloudformation list-stacks --output text | cut -f6`
+for stack in `aws --region $region cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --output text | grep STACKSUMMARIES | cut -f6`
 do 
    echo "Processing CloudFormation stack $stack"
   if [[ "$stack" != *"cloud9"* ]];
