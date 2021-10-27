bb1c350d6141965cc42c54c9457ced412ad4a3fd
George Niece <george.niece@gmail.com>
2021-10-27 02:33:04 +0000

    Update snapshot handling

--- deleteEc2Snapshots.sh
+++ deleteEc2Snapshots.sh
@@ -1,6 +1,6 @@
 for region in  us-east-1 us-west-1 us-east-2 us-west-2
 do
-  echo "List EC2 Snapshots in region $region..."
+  echo "Delete EC2 Snapshots in region $region..."
    for snapshot in `aws ec2 describe-snapshots --owner-ids self --output text --region $region | cut -f6`
    do
 	aws ec2 delete-snapshot --snapshot-id $snapshot
