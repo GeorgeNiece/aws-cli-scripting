2c87f08a779cef2de7e9d7bafc5bf3cee488b3cb
George Niece <george.niece@gmail.com>
2021-10-28 00:54:57 +0000

    Security Group Rules update

--- deleteEc2SecurityGroups.sh
+++ deleteEc2SecurityGroups.sh
@@ -7,8 +7,9 @@ do
        		  echo "aws ec2 delete-security-group --group-id $sg2"
        		  echo "aws ec2 revoke-security-group-ingress --group-id $sg2 --ip-permissions  "`aws ec2 describe-security-groups --output json --group-ids $sg2 --query "SecurityGroups[0].IpPermissions"`""
        		  aws ec2 revoke-security-group-ingress --group-id $sg2 --ip-permissions  "`aws ec2 describe-security-groups --output json --group-ids $sg2 --query "SecurityGroups[0].IpPermissions"`"
-		  aws ec2 revoke-security-group-ingress --group-id $sg2
-       		  aws ec2 revoke-security-group-egress --group-id $sg2
+       		  echo "aws ec2 revoke-security-group-egress --group-id $sg2 --ip-permissions  "`aws ec2 describe-security-groups --output json --group-ids $sg2 --query "SecurityGroups[0].IpPermissions"`""
+       		  aws ec2 revoke-security-group-egress --group-id $sg2 --ip-permissions  "`aws ec2 describe-security-groups --output json --group-ids $sg2 --query "SecurityGroups[0].IpPermissions"`"
+       		  echo "aws ec2 delete-security-group --group-id $sg2"
        		  aws ec2 delete-security-group --group-id $sg2
 		done
  fi
